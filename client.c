0f333883aba45ce9f5a65f1726e1b0bb05898ec0
felixjimenez4 <felixjimenez4444@gmail.com>
2024-06-03 14:37:40 -0500

    init

---
+++ client.c
@@ -0,0 +1,7 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
---
+++ server.c
@@ -0,0 +1,10 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+
